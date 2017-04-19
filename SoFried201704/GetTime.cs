4611e6a57e43d08f7204459e36ecc20197d73033
Kelly Ford <kford@dnndev.com>
2017-04-19 16:23:39 -0700

    initial commit

---
+++ SoFried201704/GetTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dnn.PersonaBar.Prompt.Models;
+using DotNetNuke.Entities.Portals;
+using Dnn.PersonaBar.Prompt.Common;
+using Dnn.PersonaBar.Prompt.Interfaces;
+using DotNetNuke.Entities.Users;
+using Dnn.PersonaBar.Prompt.Attributes;
+
+namespace KellyFord.Prompt.Commands
+{
+    [ConsoleCommand("get-time", "kf", "Returns the Server Time", new string[] { })]
+    class GetTime : ConsoleCommandBase, IConsoleCommand
+    {
+        public string ValidationMessage { get; }
+
+        public void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
+        {
+            base.Initialize(args, portalSettings, userInfo, activeTabId);
+        }
+
+        public bool IsValid()
+        {
+            return string.IsNullOrEmpty(ValidationMessage);
+        }
+
+        public ConsoleResultModel Run()
+        {
+            return new ConsoleResultModel("Success!");
+        }
+    }
+}
