cb3aa60b296a11e9be16506a8fcc7bc56c3dbaf0
Kelly Ford <kford@dnndev.com>
2017-04-19 18:12:21 -0700

    Add --format flag to format the resulting date and time.

--- SoFried201704/GetTime.cs
+++ SoFried201704/GetTime.cs
@@ -10,11 +10,32 @@ namespace KellyFord.Prompt.Commands
     [ConsoleCommand("get-time", "kf", "Returns the Server Time", new string[] { })]
     public class GetTime : ConsoleCommandBase, IConsoleCommand
     {
+        private const string FLAG_FORMAT = "format";
+        public string Format { get; private set; }
+
         public string ValidationMessage { get; }
 
         public void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
         {
             base.Initialize(args, portalSettings, userInfo, activeTabId);
+
+            // Format Flag:
+            // ------------------
+
+            // If Flag exists, read it
+            // use base class' HasFlag function to determine if flag was passed
+            if (HasFlag(FLAG_FORMAT))
+            {
+                // use base class' Flag() function to retrieve the value.
+                // we're passing in "F" to use if the Flag doesn't have a value
+                // "F" will do a full format on the date and time.
+                Format = Flag(FLAG_FORMAT, "F");
+            }
+            else
+            {
+                // no Format flag found - use a default of 'F' for full date formatting
+                Format = "F";
+            }
         }
 
         public bool IsValid()
@@ -24,7 +45,7 @@ namespace KellyFord.Prompt.Commands
 
         public ConsoleResultModel Run()
         {
-            return new ConsoleResultModel(System.DateTime.Now.ToString()) { };
+            return new ConsoleResultModel(System.DateTime.Now.ToString(Format)) { };
         }
     }
 }
